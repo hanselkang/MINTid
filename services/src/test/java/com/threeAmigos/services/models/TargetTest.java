97e1628942f8414e0b9e62ea0c7a4f49ce56c9f6
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-08-13 15:52:37 +0100

    test TargetTest > getTarget()

--- services/src/test/java/com/threeAmigos/services/models/TargetTest.java
+++ services/src/test/java/com/threeAmigos/services/models/TargetTest.java
@@ -3,19 +3,25 @@ package com.threeAmigos.services.models;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.Assert.*;
 
 public class TargetTest {
 
     private Target target1;
-
+    private LocalDate date1;
 
     @Before
-    public void setUp() throws Exception {}
+    public void setUp() throws Exception {
+        date1 = LocalDate.of(2022, 8, 31);
+        target1 = new Target("Buy House", 2000000,date1);
+    }
 
 
     @Test
     public void getTarget() {
+        assertEquals(2000000,target1.getTarget());
     }
 
     @Test
