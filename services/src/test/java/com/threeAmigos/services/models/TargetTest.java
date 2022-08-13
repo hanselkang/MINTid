36a3d8430355673da4ca00ab7ce67b8418dcf36a
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-08-13 15:38:07 +0100

    Make Target test

---
+++ services/src/test/java/com/threeAmigos/services/models/TargetTest.java
@@ -0,0 +1,40 @@
+package com.threeAmigos.services.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TargetTest {
+
+    private Target target1;
+
+
+    @Before
+    public void setUp() throws Exception {}
+
+
+    @Test
+    public void getTarget() {
+    }
+
+    @Test
+    public void setTarget() {
+    }
+
+    @Test
+    public void getDate() {
+    }
+
+    @Test
+    public void setDate() {
+    }
+
+    @Test
+    public void getTarget_name() {
+    }
+
+    @Test
+    public void setTarget_name() {
+    }
+}
